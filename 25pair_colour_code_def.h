3fe5c8ede20d0a16c5e3406eb5cace6feea71d33
Velayuthaperumal <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-01-10 19:39:52 +0530

    Splitted the files and rename main.c

---
+++ 25pair_colour_code_def.h
@@ -0,0 +1,22 @@
+#ifndef __25_PAIR_COLOUR_CODE_H_
+#define __25_PAIR_COLOUR_CODE_H_
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+#endif
